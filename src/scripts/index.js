8d4ab3699771911e4b877f725ffa0d30e2f27f8a
Wendi Kardian <69452468+wendikardian@users.noreply.github.com>
2023-05-19 00:08:04 +0700

    update

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,48 +1,63 @@
-import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.scss';
-import '../styles/responsive.scss';
-import dataJSON from '../public/data/DATA.json';
+import "regenerator-runtime"; /* for async await transpile */
+import "../styles/main.scss";
+import "../styles/responsive.scss";
+import dataJSON from "../public/data/DATA.json";
 
+const menu = document.querySelector("#menu");
+const menuDiv = document.querySelector("#mobile-menu");
+const main = document.querySelector("main");
 
-const menu = document.querySelector('#menu');
-const menuDiv = document.querySelector('#mobile-menu');
+menu.addEventListener("click", function (event) {
+  // console.log(menuDiv)
+  menuDiv.classList.toggle("open");
+  event.stopPropagation();
 
+  // menuDiv.classList.toggle('flex');
+});
 
-menu.addEventListener('click', function () {
-    // console.log(menuDiv)
-    menuDiv.classList.toggle('open');
-    // menuDiv.classList.toggle('flex');
-    }
-);
+main.addEventListener("click", function () {
+  menuDiv.classList.remove("open");
+});
 
-const data = Object.values(dataJSON)
+const data = Object.values(dataJSON);
 
-const rest = document.querySelector('#restaurant-container');
+const rest = document.querySelector("#restaurant-container");
 
-data[0].forEach(item => {
-    console.log(item)
-    const divParent = document.createElement('div');
-    divParent.setAttribute('tabindex', 0)
-    divParent.classList.add('card');
-    const button = document.createElement('button');
-    button.classList.add('card-button');
-    button.textContent = 'Detail';
-    const imgChild = document.createElement('img');
-    imgChild.setAttribute('alt', item.name);
-    const pTitle = document.createElement('p');
-    pTitle.className = 'card-name-restaurant';
-    const pLocation = document.createElement('p');
-    const pRating = document.createElement('p');
-    pTitle.textContent = item.name;
-    pLocation.textContent = `Location : ${item.city}`
-    pRating.textContent = `Rating : ${item.rating}`
-    imgChild.src = item.pictureId;
-    divParent.appendChild(imgChild);
-    divParent.appendChild(pTitle);
-    divParent.appendChild(pLocation);
-    divParent.appendChild(pRating);
+data[0].forEach((item) => {
+  console.log(item);
+  const divParent = document.createElement("div");
+  divParent.setAttribute("tabindex", 0);
+  divParent.classList.add("card");
+  const imgChild = document.createElement("img");
+  imgChild.classList.add("card-image");
+  imgChild.setAttribute("alt", item.name);
+  const pTitle = document.createElement("p");
+  pTitle.className = "card-name-restaurant";
+  const pLocation = document.createElement("p");
+  const pRating = document.createElement("p");
+  pTitle.textContent = item.name;
+  pLocation.textContent = `Location : ${item.city}`;
+  pRating.textContent = `Rating : ${item.rating}`;
+  imgChild.src = item.pictureId;
+  divParent.appendChild(imgChild);
+  divParent.appendChild(pTitle);
+  divParent.appendChild(pLocation);
+  divParent.appendChild(pRating);
+  rest.appendChild(divParent);
+});
 
-    divParent.appendChild(button);
-    rest.appendChild(divParent);
+const restaurantInput = document.querySelector("#restaurant-input");
+// console.log(restaurantInput);
 
+data[0].forEach((item) => {
+  const option = document.createElement("option");
+  option.setAttribute("value", item.name);
+  option.textContent = item.name;
+  restaurantInput.appendChild(option);
+});
+
+const button = document.querySelector(".card-button");
+button.addEventListener("click", (event) => {
+  event.preventDefault();
+  alert("thanks for booking");
 });
